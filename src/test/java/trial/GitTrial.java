0ea21d90e20c1985e140727feff4319aa50558bb
asmitarabole07 <asmitarabole07@gmail.com>
2021-08-11 11:28:14 +0530

    method update

--- src/test/java/trial/GitTrial.java
+++ src/test/java/trial/GitTrial.java
@@ -5,8 +5,18 @@ import org.testng.annotations.Test;
 public class GitTrial {
 	
 	@Test
-	public void name() {
+	public void trial1() {
 		System.out.println("GitPushTrial");
+	}
+	
+	@Test
+	public void trial2() {
+		System.out.println("trial 2");
+	}
+	
+	@Test
+	public void trial3() {
+		System.out.println("trial 3");
 	}
 }
 
